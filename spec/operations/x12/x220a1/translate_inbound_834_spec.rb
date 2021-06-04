27ef02f241ef104eb0b1f7de0ad5164aad50617f
Trey Evans <lewis.r.evans@gmail.com>
2021-06-03 23:56:32 -0400

    XML transformation is done.
    Will still need to wait to see how we are going to absorb the event.

--- spec/operations/x12/x220a1/translate_inbound_834_spec.rb
+++ spec/operations/x12/x220a1/translate_inbound_834_spec.rb
@@ -59,8 +59,97 @@ RSpec.describe X12::X220A1::TranslateInbound834, "given:
     described_class.new.call(payload)
   end
 
-  it "fails to parse" do
+  it "fails to validate" do
     expect(subject.success?).to be_falsey
     expect(subject.failure).not_to eq :parse_payload_failed
+  end
+end
+
+RSpec.describe X12::X220A1::TranslateInbound834, "given a full XML with almost everything" do
+  let(:payload) do
+    <<-XMLCODE
+    <X12_005010X220A1_834A1 xmlns="urn:x12:schemas:005:010:834A1A1:BenefitEnrollmentAndMaintenance">
+      <ST_TransactionSetHeader>
+        <ST01__TransactionSetIdentifierCode>834</ST01__TransactionSetIdentifierCode>
+        <ST02__TransactionSetControlNumber>12345</ST02__TransactionSetControlNumber>
+        <ST03__ImplementationConventionReference>X220A1</ST03__ImplementationConventionReference>
+      </ST_TransactionSetHeader>
+      <BGN_BeginningSegment>
+        <BGN01__TransactionSetPurposeCode>00</BGN01__TransactionSetPurposeCode>
+        <BGN02__TransactionSetReferenceNumber>12345</BGN02__TransactionSetReferenceNumber>
+        <BGN03__TransactionSetCreationDate>20200115</BGN03__TransactionSetCreationDate>
+        <BGN04__TransactionSetCreationTime>18201032</BGN04__TransactionSetCreationTime>
+        <BGN05__TimeZoneCode>UT</BGN05__TimeZoneCode>
+        <!-- Optional -->
+        <BGN06__OriginalTransactionSetReferenceNumber>3210</BGN06__OriginalTransactionSetReferenceNumber>
+        <BGN08__ActionCode>2</BGN08__ActionCode>
+      </BGN_BeginningSegment>
+      <Loop_1000A>
+        <N1_SponsorName_1000A>
+          <N101__EntityIdentifierCode>P5</N101__EntityIdentifierCode>
+          <N102__PlanSponsorName>An Employer or Exchange</N102__PlanSponsorName>
+          <N103__IdentificationCodeQualifier>FI</N103__IdentificationCodeQualifier>
+          <N104__SponsorIdentifier>123456789</N104__SponsorIdentifier>
+        </N1_SponsorName_1000A>
+      </Loop_1000A>
+      <Loop_1000B>
+        <N1_Payer_1000B>
+          <N101__EntityIdentifierCode>IN</N101__EntityIdentifierCode>
+          <N102__InsurerName>A Carrier</N102__InsurerName>
+          <N103__IdentificationCodeQualifier>FI</N103__IdentificationCodeQualifier>
+          <N104__InsurerIdentificationCode>123456789</N104__InsurerIdentificationCode>
+        </N1_Payer_1000B>
+      </Loop_1000B>
+      <Loop_1000C>
+        <N1_TPABrokerName_1000C>
+          <N101__EntityIdentifierCode>BO</N101__EntityIdentifierCode>
+          <N102__TPAOrBrokerName>A Broker</N102__TPAOrBrokerName>
+          <N103__IdentificationCodeQualifier>FI</N103__IdentificationCodeQualifier>
+          <N104__TPAOrBrokerIdentificationCode>123456789</N104__TPAOrBrokerIdentificationCode>
+        </N1_TPABrokerName_1000C>
+      </Loop_1000C>
+      <Loop_1000C>
+        <N1_TPABrokerName_1000C>
+          <N101__EntityIdentifierCode>TV</N101__EntityIdentifierCode>
+          <N102__TPAOrBrokerName>A TPA</N102__TPAOrBrokerName>
+          <N103__IdentificationCodeQualifier>FI</N103__IdentificationCodeQualifier>
+          <N104__TPAOrBrokerIdentificationCode>123456789</N104__TPAOrBrokerIdentificationCode>
+        </N1_TPABrokerName_1000C>
+      </Loop_1000C>
+      <Loop_2000>
+      <INS_MemberLevelDetail_2000>
+        <INS01__MemberIndicator>Y</INS01__MemberIndicator>
+        <INS03__MaintenanceTypeCode>021</INS03__MaintenanceTypeCode>
+        <INS04__MaintenanceReasonCode>EC</INS04__MaintenanceReasonCode>
+      </INS_MemberLevelDetail_2000>
+      <REF_SubscriberIdentifier_2000>
+        <REF01__ReferenceIdentificationQualifier>0F</REF01__ReferenceIdentificationQualifier>
+        <REF02__SubscriberIdentifier>SUBID</REF02__SubscriberIdentifier>
+      </REF_SubscriberIdentifier_2000>
+      <REF_MemberSupplementalIdentifier_2000>
+        <REF01__ReferenceIdentificationQualifier>17</REF01__ReferenceIdentificationQualifier>
+        <REF02__MemberSupplementalIdentifier>SUPPLEMENTAL_ID_1</REF02__MemberSupplementalIdentifier>
+      </REF_MemberSupplementalIdentifier_2000>
+      <REF_MemberSupplementalIdentifier_2000>
+        <REF01__ReferenceIdentificationQualifier>ZZ</REF01__ReferenceIdentificationQualifier>
+        <REF02__MemberSupplementalIdentifier>SUPPLEMENTAL_ID_2</REF02__MemberSupplementalIdentifier>
+      </REF_MemberSupplementalIdentifier_2000>
+      <DTP_MemberLevelDates_2000>
+        <DTP01__DateTimeQualifier>303</DTP01__DateTimeQualifier>
+        <DTP02__DateTimePeriodFormatQualifier>D8</DTP02__DateTimePeriodFormatQualifier>
+        <DTP03__StatusInformationEffectiveDate>20200315</DTP03__StatusInformationEffectiveDate>
+      </DTP_MemberLevelDates_2000>
+
+    </Loop_2000>
+    </X12_005010X220A1_834A1>
+    XMLCODE
+  end
+
+  subject do
+    described_class.new.call(payload)
+  end
+
+  it "succeeds" do
+    expect(subject.success?).to be_truthy
   end
 end
