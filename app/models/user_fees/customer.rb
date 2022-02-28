ca2b67680e45571705745d3beaaf745887ea3784
ipublic <dann@ideacrew.com>
2022-02-27 22:16:17 -0500

    Enable keepr gem Move to Customer/Account as ActiveRecord, InsuranceCoverage and child models as ActiveModel

---
+++ app/models/user_fees/customer.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module UserFees
+  # A person on the ACA Individual Market who is responsible for paying a family group's insurance premiums
+  class Customer < ::ApplicationRecord
+    belongs_to :account, class_name: '::Keepr::Account'
+    validates_associated :account
+    validates :insurance_coverage_id, presence: true
+    validate :insurance_coverage_is_valid
+
+    after_save :persist_insurance_coverage
+
+    # around_save :persist_insurance_coverage
+
+    def insurance_coverage
+      return @insurance_coverage if defined?(@insurance_coverage)
+      @insurance_coverage = ::UserFees::InsuranceCoverage.find(insurance_coverage_id) unless insurance_coverage_id.nil?
+    end
+
+    # Creates a 'has_one' association between an ActiveRecord UserFees::Customer and a Mongoid ActiveModel
+    #   {UserFees::InsuranceCoverage} instance
+    # @param [Hash] opts the options for referencing the InsuranceCoverage instance
+    # @param opts [Hash] :obj a hash that will validate using: {::AcaEntities::Ledger::Contracts::InsuranceCoverageContract}
+    # @param opts [::UserFees::InsuranceCoverage] :obj an instance of {UserFees::InsuranceCoverage}
+    def insurance_coverage=(obj)
+      coverage = obj if obj.is_a?(::UserFees::InsuranceCoverage)
+      coverage = ::UserFees::InsuranceCoverage.new(obj) if obj.is_a?(Hash)
+      raise ArgumentError, 'expected ::UserFees::InsuranceCoverage or Hash' unless coverage.present?
+      self.insurance_coverage_id = coverage.id.to_s
+      @insurance_coverage = coverage
+    end
+
+    private
+
+    def insurance_coverage_is_valid
+      return if insurance_coverage.nil?
+      errors.add(:insurance_coverage_id, insurance_coverage.errors) if insurance_coverage.invalid?
+    end
+
+    def persist_insurance_coverage
+      insurance_coverage.save if insurance_coverage.changed? || insurance_coverage.children_changed?
+    end
+  end
+end
---
+++ spec/shared_examples/user_fees/customer_params.rb
@@ -0,0 +1,191 @@
+# frozen_string_literal: true
+
+RSpec.shared_context 'customer_params' do
+  let(:is_active) { true }
+  let(:moment) { DateTime.now }
+  let(:timestamps) { { created_at: moment, modified_at: moment } }
+
+  # Member
+  let(:hbx_id) { '1138345' }
+  let(:subscriber_hbx_id) { hbx_id }
+  let(:person_name) { { first_name: 'George', last_name: 'Jetson' } }
+  let(:member) { { hbx_id: hbx_id, subscriber_hbx_id: subscriber_hbx_id, person_name: person_name } }
+
+  # Product
+  let(:hbx_qhp_id) { '96667ME031005806' }
+  let(:effective_year) { 2022 }
+  let(:kind) { 'health' }
+  let(:product) { { hbx_qhp_id: hbx_qhp_id, effective_year: effective_year, kind: kind } }
+
+  # Premium
+  let(:insured_age) { 33 }
+  let(:amount) { 875.22 }
+  let(:premium) { { insured_age: insured_age, amount: amount } }
+
+  # Enrolled Member
+  let(:start_on) { Date.new(2022, 2, 1) }
+  let(:enrolled_member) { { member: member, premium: premium, start_on: start_on } }
+  let(:enrolled_members) { [enrolled_member] }
+
+  # Insurer
+  let(:hios_id) { '96667' }
+  let(:insurer) { { hios_id: hios_id } }
+
+  # Policy
+  let(:exchange_assigned_id) { '68576' }
+  let(:rating_area_id) { 'R-ME001' }
+  let(:policy) do
+    {
+      exchange_assigned_id: exchange_assigned_id,
+      marketplace_segments: marketplace_segments,
+      subscriber_hbx_id: subscriber_hbx_id,
+      insurer: insurer,
+      product: product,
+      rating_area_id: rating_area_id,
+      start_on: start_on
+    }
+  end
+  let(:policies) { [policy] }
+
+  # Marketplace Segment
+  let(:policy_id) { '68576' }
+  let(:segment) { [subscriber_hbx_id, policy_id, start_on.strftime('%Y%m%d')].join('-') }
+  let(:marketplace_segment) do
+    {
+      subscriber_hbx_id: subscriber_hbx_id,
+      start_on: start_on,
+      segment: segment,
+      enrolled_members: enrolled_members,
+      total_premium_amount: amount,
+      total_premium_responsibility_amount: amount
+    }
+  end
+  let(:marketplace_segments) { [marketplace_segment] }
+
+  # Tax Household
+  let(:tax_household) { { aptc_amount: 585.69 } }
+  let(:tax_households) { [tax_household] }
+
+  # InsuranceCoverage
+  let(:insurance_coverage) { { tax_households: tax_households, policies: policies, is_active: is_active } }
+
+  # All Attributes in Transaction form
+  let(:transaction) do
+    {
+      # customer_id: '1055668',
+      hbx_id: '1055668',
+      last_name: 'Jetson',
+      first_name: 'George',
+      customer_role: 'subscriber',
+      is_active: true,
+      account: {
+        number: '1100001'.to_i,
+        name: 'Accounts Receivable',
+        kind: 'asset',
+        is_active: true
+      },
+      insurance_coverage: {
+        tax_households: [{ id: '100', aptc_amount: 850.0, csr: 0, start_on: '20220101', end_on: '20221231' }],
+        policies: [
+          {
+            exchange_assigned_id: '50836',
+            insurer_assigned_id: 'HP5977620',
+            rating_area_id: 'R-ME003',
+            subscriber_hbx_id: '1055668',
+            start_on: '20220101',
+            end_on: '20221231',
+            insurer: {
+              hios_id: '96667'
+            },
+            product: {
+              hbx_qhp_id: '96667ME031005806',
+              effective_year: 2022,
+              kind: 'health'
+            },
+            marketplace_segments: [
+              {
+                segment: '1055668-50836-20220101',
+                total_premium_amount: 1104.58,
+                total_premium_responsibility_amount: 254.58,
+                start_on: '20220101',
+                enrolled_members: [
+                  {
+                    member: {
+                      relationship_code: '1:18',
+                      is_subscriber: true,
+                      subscriber_hbx_id: '1055668',
+                      hbx_id: '1055668',
+                      insurer_assigned_id: 'HP597762000',
+                      insurer_assigned_subscriber_id: 'HP597762000',
+                      person_name: {
+                        last_name: 'Jetson',
+                        first_name: 'George'
+                      },
+                      ssn: '012859874',
+                      dob: '19781219',
+                      gender: 'male',
+                      tax_household_id: '100'
+                    },
+                    premium: {
+                      amount: 423.86
+                    },
+                    start_on: '20220101',
+                    end_on: '20221231'
+                  },
+                  {
+                    member: {
+                      relationship_code: '4:19',
+                      is_subscriber: false,
+                      subscriber_hbx_id: '1055668',
+                      hbx_id: '1055678',
+                      insurer_assigned_id: 'HP597762002',
+                      insurer_assigned_subscriber_id: 'HP597762000',
+                      person_name: {
+                        last_name: 'Jetson',
+                        first_name: 'Jane'
+                      },
+                      ssn: '012859875',
+                      dob: '19830906',
+                      gender: 'female',
+                      tax_household_id: '100'
+                    },
+                    premium: {
+                      amount: 410.06
+                    },
+                    start_on: '20220101',
+                    end_on: '20221231'
+                  },
+                  {
+                    member: {
+                      relationship_code: '2:01',
+                      is_subscriber: false,
+                      subscriber_hbx_id: '1055668',
+                      hbx_id: '1055689',
+                      insurer_assigned_id: 'HP597762001',
+                      insurer_assigned_subscriber_id: 'HP597762000',
+                      person_name: {
+                        last_name: 'Jetson',
+                        first_name: 'Judy'
+                      },
+                      ssn: '012859876',
+                      dob: '20070215',
+                      gender: 'female',
+                      tax_household_id: '100',
+                      emails: 'jetsons@example.com'
+                    },
+                    premium: {
+                      amount: 270.66
+                    },
+                    start_on: '20220101',
+                    end_on: '20221231'
+                  }
+                ]
+              }
+            ]
+          }
+        ],
+        is_active: true
+      }
+    }
+  end
+end
