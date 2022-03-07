f89ed4fc5f9ce8e4685ef465a655a432649beeb2
ipublic <dann@ideacrew.com>
2022-03-07 15:09:34 -0500

    Processing new customers and new policies

--- app/operations/user_fees/gdb_transactions/check_addition_transaction.rb
+++ app/operations/user_fees/gdb_transactions/check_addition_transaction.rb
@@ -9,59 +9,125 @@ module UserFees
     #
     class CheckAdditionTransaction
       include Dry::Monads[:result, :do, :try]
+      include EventSource::Command
 
       # @param [Hash] params the parameters to resolve message type
       # @option params [Hash] :message EDI Database transaction (required)
-      # @return [Dry::Monad::Success] message is a addition
-      # @return [Dry::Monad::Failure] message is not a addition
+      # @return [Dry::Monads::Success] message is a addition
+      # @return [Dry::Monads::Failure] message is not a addition
       def call(params)
         message = yield validate(params)
-        result = yield classify_transaction(message)
+        events = yield inspect_transaction(message)
+        published_events = yield publish_events(events)
 
-        Success(result)
+        Success(published_events)
       end
 
       private
 
       def validate(message)
-        return Success(message) if message.is_an_instance_of Hash
+        return Success(message) if message.is_a? Hash
         Failure('hash expected')
       end
 
-      def classify_transaction(message)
-        binding.pry
-        hbx_id = message[:customer][:hbx_id]
-        new_customer = detect_new_customer(message[:customer])
-        new_policies = detect_new_policies(message[:customer])
-        new_tax_households = detect_new_tax_households(message[:customer])
-        new_enrolled_members = detect_new_enrolled_members(message[:customer])
+      def inspect_transaction(message)
+        customer_new_state = (message[:customer])
+        customer_state = fetch_customer(customer_new_state)
+
+        return Success([detect_new_customer(customer_state, customer_new_state)]) unless customer_state.present?
+
+        policy_events = detect_new_policies(customer_state, customer_new_state)
+        _tax_household_events = detect_new_tax_households(customer_state, customer_new_state)
+        _enrolled_member_events = detect_new_enrollment_members(customer_state, customer_new_state)
+
+        Success([policy_events]) #, tax_household_events, enrolled_member_events].compact)
+      rescue StandardError => e
+        Failure("error parsing transaction:\n    #{customer_new_state}\n    #{e}")
+        # Failure("error parsing transaction:\n    #{customer_new_state}\n    #{e.backtrace}")
       end
 
-      # add initial policy
-      def detect_new_customer(customer)
-        ::UserFees::Customer.find_by(hbx_id: customer[:hbx_id]) ? nil : customer
+      def publish_events(events)
+        Success(events.each { |event| event.success.publish })
+      rescue StandardError => e
+        Failure("error publshing events:\n    #{e}\n    events: #{events}\n    backtrace: #{e.backtrace}")
       end
 
-      # add policies
-      def detect_new_policies(customer)
-        customer[:policies].reduce([]) do |new_policies, policy|
-          new_policies << policy unless customer_policy_exists?(customer, policy)
-        end
+      def fetch_customer(customer)
+        ::UserFees::Customer.find_by(hbx_id: customer[:hbx_id]) || {}
       end
 
-      def customer_policy_exists?(customer, policy)
-        ::UserFee::InsuranceCoverage.policy_id(customer, policy)
+      # initial enrollment added
+      def detect_new_customer(customer_state, customer_new_state)
+        build_event('initial_enrollment_added', {}, customer_state, customer_new_state) if customer_state.empty?
       end
 
-      # add tax households
-      def detect_new_tax_households(customer)
-        customer[:tax_households].reduce([]) do |new_tax_households, tax_household|
-          new_tax_households << tax_household unless customer_tax_household_exists?(hbx_id, message[:customer])
-        end
+      # policies added
+      def detect_new_policies(customer_state, customer_new_state)
+        policies = customer_state.insurance_coverage.policies || []
+        new_policies = customer_new_state.dig(:insurance_coverage, :policies) || []
+
+        new_policy_set =
+          new_policies.reduce([]) do |list, np|
+            list << np if policies.none? { |policy| policy.exchange_assigned_id == np.fetch(:exchange_assigned_id) }
+            list
+          end
+
+        build_event('policies_added', new_policy_set, customer_state, customer_new_state) || []
       end
 
-      def customer_tax_household_exists?(customer, tax_household)
-        ::UserFee::InsuranceCoverage.tax_household(customer, tax_household)
+      # tax households added
+      def detect_new_tax_households(customer_state, customer_new_state)
+        tax_hhs = customer_state.insurance_coverage.tax_households || []
+        new_tax_hhs = customer_new_state.dig(:insurance_coverage, :tax_households) || []
+
+        new_tax_household_set =
+          new_tax_hhs.reduce([]) do |list, nthh|
+            list << nthh if tax_hhs.none? { |tax_hh| tax_hh.exchange_assigned_id == nthh.fetch(:exchange_assigned_id) }
+            list
+          end
+        build_event('tax_households_added', new_tax_household_set, customer_state, customer_new_state) || []
+      end
+
+      # enrollment_members added
+      def detect_new_enrollment_members(customer_state, customer_new_state)
+        []
+        # tax_hhs = customer_state.dig(:insurance_coverage, :policies) || []
+        # new_tax_hhs = customer_new_state.dig(:insurance_coverage, :policies) || []
+        # new_tax_household_set =
+        #   new_tax_hhs.reduce([]) do |list, new_tax_hh|
+        #     if tax_hhs.none? { |tax_hh| tax_hh[:exchange_assigned_id] == new_tax_hh[:exchange_assigned_id] }
+        #       list << new_thh
+        #     end
+        #   end
+        # build_event('enrollment_members_added', customer, new_tax_household_set)
+      end
+
+      def build_event(event_name, change_set, customer_state, customer_new_state)
+        event_namespace = 'events.user_fees.enrollment_adds'
+        full_event_name = [event_namespace, event_name].join('.')
+        meta = build_meta_content(change_set, customer_new_state)
+        attributes = {
+          meta: meta,
+          old_state: {
+            customer: customer_state
+          },
+          new_state: {
+            customer: customer_new_state
+          }
+        }
+
+        event(full_event_name, attributes: attributes)
+      end
+
+      def build_meta_content(change_set, customer_new_state)
+        correlation_id = AcaEntities::Types::CorrelationId
+        time = DateTime.now
+        {
+          correlation_id: correlation_id,
+          time: time,
+          customer_hbx_id: customer_new_state[:hbx_id],
+          change_set: change_set
+        }
       end
     end
   end
--- spec/operations/user_fees/gdb_transactions/check_addition_transaction_spec.rb
+++ spec/operations/user_fees/gdb_transactions/check_addition_transaction_spec.rb
@@ -7,24 +7,138 @@ require 'shared_examples/user_fees/gdb_transactions/add_policy'
 require 'shared_examples/user_fees/gdb_transactions/add_enrolled_member'
 
 RSpec.describe UserFees::GdbTransactions::CheckAdditionTransaction do
-  subject { described_class }
+  subject { described_class.new }
   include_context 'initial_transaction'
+  include_context 'add_policy'
+  include_context 'add_enrolled_member'
 
-  context 'Given a new Customer transaction' do
+  context 'Given a transaction for a new Customer ' do
     let(:customer) { jetson_initial_transaction }
+    let(:old_state) { { customer: {} } }
+    let(:hbx_id) { customer.dig(:customer, :hbx_id) }
 
-    it 'should generate an initial_enrollment_added event'
+    it 'should generate an initial_enrollment_added event' do
+      result = subject.call(customer)
 
-    context 'and a new policy is added' do
-      let(:customer_dental_policy) { jetson_add_dental_policy }
-
-      it 'should generate an enrollment_policy_added event'
-
-      context 'and a new enrolled member is added' do
-        let(:customer_add_dependent) { jetson_add_dependent }
-
-        it 'should generate an enrollment_dependent_added event'
-      end
+      expect(result.success?).to be_truthy
+      expect(result.success.size).to eq 1
+      expect(result.success.first.success).to be_an_instance_of Events::UserFees::EnrollmentAdds::InitialEnrollmentAdded
+      expect(result.success.first.success.payload[:old_state]).to eq old_state
+      expect(result.success.first.success.payload.dig(:meta, :change_set)).to eq Hash.new
+      expect(result.success.first.success.payload.dig(:new_state, :customer, :hbx_id)).to eq hbx_id
     end
+  end
+
+  context 'Given a transaction with a new policy for an existing Customer' do
+    let(:customer_dental_policy) { jetson_add_dental_policy }
+    let(:policy_change_set) do
+      [
+        {
+          exchange_assigned_id: '50837',
+          insurer_assigned_id: 'HP5977621',
+          rating_area_id: 'R-ME003',
+          subscriber_hbx_id: '1055668',
+          start_on: '20220101',
+          end_on: '20221231',
+          insurer: {
+            hios_id: '96668'
+          },
+          product: {
+            hbx_qhp_id: '96667ME031005807',
+            effective_year: 2022,
+            kind: 'dental'
+          },
+          marketplace_segments: [
+            {
+              segment: '1055668-50837-20220101',
+              total_premium_amount: 0.12012e3,
+              total_premium_responsibility_amount: 0.12012e3,
+              start_on: '20220101',
+              enrolled_members: [
+                {
+                  member: {
+                    relationship_code: '1:18',
+                    is_subscriber: true,
+                    subscriber_hbx_id: '1055668',
+                    hbx_id: '1055668',
+                    person_name: {
+                      last_name: 'Jetson',
+                      first_name: 'George'
+                    },
+                    ssn: '012859874',
+                    dob: '19781219',
+                    gender: 'male'
+                  },
+                  premium: {
+                    amount: 0.4003e2
+                  },
+                  start_on: '20220101',
+                  end_on: '20221231'
+                },
+                {
+                  member: {
+                    relationship_code: '4:19',
+                    is_subscriber: false,
+                    subscriber_hbx_id: '1055668',
+                    hbx_id: '1055678',
+                    person_name: {
+                      last_name: 'Jetson',
+                      first_name: 'Jane'
+                    },
+                    ssn: '012859875',
+                    dob: '19830906',
+                    gender: 'female'
+                  },
+                  premium: {
+                    amount: 0.4007e2
+                  },
+                  start_on: '20220101',
+                  end_on: '20221231'
+                },
+                {
+                  member: {
+                    relationship_code: '2:01',
+                    is_subscriber: false,
+                    subscriber_hbx_id: '1055668',
+                    hbx_id: '1055689',
+                    person_name: {
+                      last_name: 'Jetson',
+                      first_name: 'Judy'
+                    },
+                    ssn: '012859876',
+                    dob: '20070215',
+                    gender: 'female',
+                    emails: 'jetsons@example.com'
+                  },
+                  premium: {
+                    amount: 0.4002e2
+                  },
+                  start_on: '20220101',
+                  end_on: '20221231'
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    end
+
+    before { UserFees::Customers::Create.new.call(customer: jetson_initial_transaction[:customer]) }
+
+    it 'should generate an enrollment_policy_added event' do
+      expect(::UserFees::Customer.all.size).to eq 1
+      result = subject.call(customer_dental_policy)
+
+      expect(result.success?).to be_truthy
+      expect(result.success.size).to eq 1
+      expect(result.success.first.success).to be_an_instance_of Events::UserFees::EnrollmentAdds::PoliciesAdded
+      expect(result.success.first.success.payload.dig(:meta, :change_set)).to eq policy_change_set
+    end
+  end
+
+  context 'Given a transaction with a new enrolled member for an existing Customer' do
+    let(:customer_add_dependent) { jetson_add_dependent }
+
+    it 'should generate an enrollment_dependent_added event'
   end
 end
