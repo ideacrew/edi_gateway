3e7439f7519b6553aa0dab8482a9e29ac7863beb
Sai Praveen Gudimetla <saipraveen.gudimetla@gmail.com>
2022-10-10 14:45:03 -0500

    remove timekeeper in subscriber (#11)

--- app/event_source/subscribers/irs_groups/seed_requested_event_subscriber.rb
+++ app/event_source/subscribers/irs_groups/seed_requested_event_subscriber.rb
@@ -12,27 +12,26 @@ module Subscribers
       # rubocop:disable Style/LineEndConcatenation
       # rubocop:disable Style/StringConcatenation
       subscribe(:on_built_requested_seed) do |delivery_info, _properties, payload|
-        subscriber_logger = subscriber_logger_for(:on_seed_requested)
+        subscriber_logger = subscriber_logger_for(:on_built_requested_seed)
         parsed_payload = JSON.parse(payload, symbolize_names: true)
-        result = ::Operations::IrsGroups::SeedIrsGroup.new.call({ payload: parsed_payload })
+        result = ::IrsGroups::SeedIrsGroup.new.call({ payload: parsed_payload })
         if result.success?
           subscriber_logger.info(
             'OK: :Created IRS Group successfully and acked'
           )
-          ack(delivery_info.delivery_tag)
         else
           subscriber_logger.info(
-            "Error: Unable to create IRS group; nacked due to:#{result.inspect}"
+            "Error: Unable to create IRS group; failed due to:#{result.inspect}"
           )
-          nack(delivery_info.delivery_tag)
         end
+        ack(delivery_info.delivery_tag)
 
       rescue Exception => e
         subscriber_logger.info(
           "Exception: Unable to create IRS group\n Exception: #{e.inspect}" +
             "\nBacktrace:\n" + e.backtrace.join("\n")
         )
-        nack(delivery_info.delivery_tag)
+        ack(delivery_info.delivery_tag)
       end
       # rubocop:enable Lint/RescueException
       # rubocop:enable Style/LineEndConcatenation
@@ -42,7 +41,7 @@ module Subscribers
 
       def subscriber_logger_for(event)
         Logger.new(
-          "#{Rails.root}/log/#{event}_#{TimeKeeper.date_of_record.strftime('%Y_%m_%d')}.log"
+          "#{Rails.root}/log/#{event}_#{Date.today.strftime('%Y_%m_%d')}.log"
         )
       end
     end
