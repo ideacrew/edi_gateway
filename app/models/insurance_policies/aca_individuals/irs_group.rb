c9087d648c8067100afc81979eaea7fe293b2c14
ipublic <dan@ideacrew.com>
2022-12-12 12:45:44 -0500

    Remove spurious index models

--- app/models/insurance_policies/aca_individuals/irs_group.rb
+++ app/models/insurance_policies/aca_individuals/irs_group.rb
@@ -7,7 +7,7 @@ module InsurancePolicies
       include Mongoid::Timestamps
       include DomainModelHelpers
 
-      belongs_to :insurance_policy, class_name: 'InsurancePolicies::AcaIndividuals::InsurancePolicy'
+      belongs_to :aca_individual_insurance_policy, class_name: 'InsurancePolicies::AcaIndividuals::InsurancePolicy'
       accepts_nested_attributes_for :insurance_policy
 
       belongs_to :tax_household_group, class_name: 'InsurancePolicies::AcaIndividuals::TaxHouseholdGroup'
