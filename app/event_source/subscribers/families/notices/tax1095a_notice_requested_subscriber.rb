215abd29e69991a1fbc54588c4b8de080c81610c
Vishal Kalletla <vishuk1201@gmail.com>
2023-01-18 12:23:59 -0500

    single subscriber for all enroll 1095a events

--- app/event_source/subscribers/families/notices/tax1095a_notice_requested_subscriber.rb
+++ app/event_source/subscribers/families/notices/tax1095a_notice_requested_subscriber.rb
@@ -3,22 +3,22 @@
 module Subscribers
   module Families
     module Notices
-      # Subscriber will receive void1095a_notice event from enroll to generate 1095a tax_payload
-      class Void1095aNoticeRequestedSubscriber
+      # Subscriber will receive initial1095a_notice event from enroll to generate 1095a tax_payload
+      class Tax1095aNoticeRequestedSubscriber
         include EventSource::Command
         include EventSource::Logging
         include ::EventSource::Subscriber[amqp: 'enroll.families.notices.ivl_tax1095a']
 
-        subscribe(:on_void_notice_requested) do |delivery_info, _metadata, response|
+        subscribe(:on_enroll_families_notices_ivl_tax1095a) do |delivery_info, _metadata, response|
           routing_key = delivery_info[:routing_key]
-          logger.info "Polypress: invoked Void1095aNoticeRequestedSubscriber with delivery_info:
-                      #{delivery_info} routing_key: #{routing_key}"
-
+          logger.info "Polypress: invoked Tax1095aNoticeRequestedSubscriber with delivery_info:
+                        #{delivery_info} routing_key: #{routing_key}"
           payload = JSON.parse(response, symbolize_names: true)
-          result = Success(true) # TODO: Add domain model that builds respective payload and publish event to polypress
+          result = ::Tax1095a::FetchAndPublishIrsGroups.new.call({ tax_year: payload[:tax_year],
+                                                                   tax_form_type: payload[:tax_form_type] })
 
           if result.success?
-            logger.info "Polypress: Catastrophic1095aNoticeRequestedSubscriber; acked for #{routing_key}"
+            logger.info "Polypress: Tax1095aNoticeRequestedSubscriber; acked for #{routing_key}"
           else
             errors = if result.is_a?(String)
                        result
@@ -28,14 +28,14 @@ module Subscribers
                        result.failure.errors.to_h
                      end
             logger.error(
-              "Polypress: Catastrophic1095aNoticeRequestedSubscriber_error;
+              "Polypress: Tax1095aNoticeRequestedSubscriber_error;
                       nacked due to:#{errors}; for routing_key: #{routing_key}, payload: #{payload}"
             )
           end
           ack(delivery_info.delivery_tag)
         rescue StandardError, SystemStackError => e
           logger.error(
-            "Polypress: Void1095aNoticeRequestedSubscriber_error: nacked due to backtrace:
+            "Polypress: Tax1095aNoticeRequestedSubscriber_error: nacked due to backtrace:
                   #{e.backtrace}; for routing_key: #{routing_key}, response: #{response}"
           )
           ack(delivery_info.delivery_tag)
