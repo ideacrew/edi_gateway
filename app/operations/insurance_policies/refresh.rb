5e55731824a5f51b03c34a0cc843576adf5d0126
Sai Kumar Kotagiri <saikumarkotagiri9@gmail.com>
2023-02-09 17:08:39 -0500

    add event, publisher and operation for families found_by and find_by (#38)
    * update aca_entities revision to latest
    
    * add event, publisher and operation for families found_by and find_by
    
    * use error instead of info to log errors

--- app/operations/insurance_policies/refresh.rb
+++ app/operations/insurance_policies/refresh.rb
@@ -4,24 +4,30 @@ module InsurancePolicies
   # Process 'enroll.insurance_policies.refresh_requested' event
   class Refresh
     include Dry::Monads[:result, :do]
-    # include EventSource::Command
+    include EventSource::Command
 
     # TODO: Implement business logic
     def call(params)
       refresh_period = yield validate(params)
-      result         = yield refresh(refresh_period)
+      event          = yield build_event(refresh_period)
+      result         = yield publish(event)
 
       Success(result)
     end
 
     private
 
+    def build_event(_refresh_period)
+      event('events.families.find_by_requested', attributes: {})
+    end
+
     def parsed_refresh_period(params)
       Range.new(*params['refresh_period'].split('..').map(&:to_time))
     end
 
-    def refresh(_refresh_period)
-      Success('Successfully processed event: enroll.insurance_policies.refresh_requested')
+    def publish(event)
+      event.publish
+      Success("Successfully published event: #{event.name}")
     end
 
     def validate(params)
--- spec/operations/insurance_policies/create_or_update_spec.rb
+++ spec/operations/insurance_policies/create_or_update_spec.rb
@@ -2,17 +2,17 @@
 
 require 'rails_helper'
 
-RSpec.describe InsurancePolicies::Refresh, dbclean: :after_each do
+RSpec.describe InsurancePolicies::CreateOrUpdate, dbclean: :after_each do
   subject { described_class.new.call(input_params) }
 
   describe '#call' do
     context 'with valid input params' do
-      let(:input_params) { { 'refresh_period' => '02/07/2023 09:51..02/07/2023 18:51' } }
+      let(:input_params) { {} }
 
       it 'returns a success with a message' do
         expect(
           subject.success
-        ).to eq('Successfully published event: events.families.find_by_requested')
+        ).to eq('Successfully processed event: enroll.families.found_by')
       end
     end
 
