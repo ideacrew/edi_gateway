51f4cca0f8a36d687434bc2956b2ea914bc28063
Saipraveen Gudimetla <saipraveen.gudimetla@gmail.com>
2023-07-31 14:03:32 -0400

    add missing glue models

--- app/models/money_math.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# MoneyMath module
-module MoneyMath
-  # @param [BigDecimal] val
-  def as_dollars(val)
-    BigDecimal(val.to_s).round(2)
-  end
-end
---
+++ app/models/transaction_management.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Context for management of transactions by Carriers.
+module TransactionManagement
+end
